153d5cbfc9d1993b77b31bd30e4ebc19a5ed14e9
RCinbo <raisa.carmen@inbo.be>
2024-08-01 10:02:58 +0200

    Add code to get bibliography files and merge them into one file.

--- source/R/habitatauteurs_from_docx.R
+++ source/R/habitatauteurs_from_docx.R
@@ -3,11 +3,15 @@ library(tidyverse)
 library(rprojroot)
 library(parsermd)
 library(googledrive)
+library(jsonlite)
+#library(synthesisr) might be interesting to find and remove duplicates from the
+#however, they do not allow to keep the labels that the bib items already have.
 
 #download the docx files from google drive
 folder <- "https://drive.google.com/drive/folders/10cZDtfWFMglGF4ICvBzv6HXkfsqT3eP1"
 folder_id <- drive_get(as_id(folder))
-files <- drive_ls(folder_id)
+files <- drive_ls(folder_id) %>%
+  filter(name != "literatuur")
 #check whether the receiving folder exists
 if (!file.exists(
   find_root_file("data/raw",
@@ -54,7 +58,10 @@ for (hoofdstuk in c("zilt", "kustduin", "water", "heide", "gras", "veen",
   for (x in paragrafen) {
     rmd_select(rmd, by_section(x)) %>%
     rmd_select(has_type("rmd_markdown")) %>% # do not print the headers, only the text
-    as_document() %>% str_replace("\\\\", "\\") %>% #otherwise, double backslash is printed in the md file
+    as_document() %>%
+      str_replace_all("\\\\", "\\") %>% #otherwise, double backslash is printed in the md file
+      str_replace_all("\\[", "[") %>%
+      str_replace_all("\\]", "]") %>%
     writeLines(useBytes = TRUE,
                con = find_root_file("source/bookdown/habitatauteurs_paragrafen",
                                     sprintf("%s_%s.md",hoofdstuk,x),
@@ -62,6 +69,8 @@ for (hoofdstuk in c("zilt", "kustduin", "water", "heide", "gras", "veen",
                                       has_file("EU_rapportage_habitats.Rproj")))
     }
 }
+
+
 #save habitatauteurs to txt file to be able to put it on github.
 auteurs <- readxl::read_excel(
   path = find_root_file("data/raw/habitatauteurs.xlsx",
@@ -69,3 +78,43 @@ auteurs <- readxl::read_excel(
 write_delim(auteurs,
             find_root_file("data/raw/habitatauteurs.txt",
                            criterion = has_file("EU_rapportage_habitats.Rproj")))
+
+# download and merge all bibliography files
+folder <- "https://drive.google.com/drive/folders/1iPCilJi0VX6qtMduInqvmR186N6YGffw"
+folder_id <- drive_get(as_id(folder))
+files <- drive_ls(folder_id)
+
+if (!file.exists(
+  find_root_file("data/raw/literatuur",
+                 criterion = has_file("EU_rapportage_habitats.Rproj")))) {
+  dir.create(find_root_file("data/raw/literatuur",
+                            criterion =
+                              has_file("EU_rapportage_habitats.Rproj")))
+}
+
+combined_bib <- ""
+
+for (i in seq_along(files %>%
+                    filter(str_detect(name, pattern = "\\.bib")) %>%
+                    dplyr::pull(name))) {
+  drive_download(
+    as_id(files$id[i]),
+    path =
+      find_root_file("data/raw/literatuur", files$name[i],
+                     criterion = has_file("EU_rapportage_habitats.Rproj")),
+    overwrite = TRUE
+  )
+  fileCon <- file(
+    find_root_file("data/raw/literatuur", files$name[i],
+                   criterion = has_file("EU_rapportage_habitats.Rproj")))
+  content <- readLines(fileCon)
+  close(fileCon)
+
+  combined_bib <- paste0(combined_bib, "\n", "\n", trimws(paste0(content, collapse="\n")))
+}
+
+cat(combined_bib,
+    file = find_root_file("source", "bookdown", "combined_references.bib",
+                          criterion = has_file("EU_rapportage_habitats.Rproj")),
+    "\n")
+
